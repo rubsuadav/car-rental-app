6d06728765284f69a4c0dda542f67fafa14c597e
rubsuadav <rubsuadav@alum.us.es>
2024-11-17 13:36:00 +0100

    Add proxy configuration for eviting CORS error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
+// local imports
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
   ],
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://api.example.com/api/CarRentalApp';
+export const API_URL = '/api/CarRentalApp';
